9a172741722eca6391302f208c673f3afe818638
Kfir Breger <kfir@cubicalwave.com>
2018-06-14 13:51:04 +0200

    Adding an interactions file to seperate from the graphics code

---
+++ interactions.js
@@ -0,0 +1,29 @@
+(function(window, document, undefined) {
+  // Adding click listeners to the buttons
+  const menuItems = document.getElementsByClassName('js-sec-switch');
+  for (let item of menuItems) {
+    item.addEventListener('click', function(event) {
+      event.preventDefault();
+      event.stopImmediatePropagation();
+      activateSection(item.getAttribute('href'));
+    }, true);
+  }
+
+  function hideAll() {
+    // Hides all the art sections
+    for (let section of document.getElementsByClassName('art-section active')) {
+      section.classList.remove('active');
+      section.classList.add('hidden');
+    }
+  }
+  function activateSection(sectionId) {
+    // Deactivates the other sections and activates the section id
+    hideAll();
+
+    sectionId = sectionId.replace('#', '');  // Removing # if it is also given in the Id
+    // Adding active to the selected item
+    section = document.getElementById(sectionId);
+    section.classList.add('active');
+    section.classList.remove('hidden');
+  }
+})(window, document);
